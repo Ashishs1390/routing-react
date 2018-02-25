c626d1739b6c64ac7b79c3b687248a066178dd45
Ashish Sharma <ashishhsharma1990@gmail.com>
2018-02-25 13:52:44 +0530

    Ashish: react start template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hello , goodbye } from './lib'
+
+ReactDOM.render(
+	<div>
+		{hello}
+		{goodbye}
+	</div>,
+	document.getElementById('react-container')
+
+)
